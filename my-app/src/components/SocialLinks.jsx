8e0ced2820dd190bfd340d86ccd394b05d6767b9
Hob <93816506+HobbesChiong@users.noreply.github.com>
2023-07-03 20:46:33 -0700

    social links finished

--- my-app/src/components/SocialLinks.jsx
+++ my-app/src/components/SocialLinks.jsx
@@ -5,19 +5,63 @@ import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const SocialLinks = () => {
 
+    const links = [
+        {
+            id:1,
+            child:(
+                <>
+                    LinkedIn <FaLinkedin size={30}/>
+                </>
+            ),
+            href: 'https://www.linkedin.com/in/hobbes-chiong/',
+            style: 'rounded-tr-md'
+        },
+        {
+            id:2,
+            child:(
+                <>
+                    GitHub <FaGithub size={30}/>
+                </>
+            ),
+            href: 'https://github.com/HobbesChiong',
+        },
+        {
+            id:3,
+            child:(
+                <>
+                    Mail <HiOutlineMail size={30}/>
+                </>
+            ),
+            href: 'mailto:hobbeschiong@gmail.com',
+        },
+        {
+            id:4,
+            child:(
+                <>
+                    Resume <BsFillPersonLinesFill size={30}/>
+                </>
+            ),
+            href: '/resume.pdf',
+            style: 'rounded-br-md',
+            download: true,
+            
+        },
 
+    ]
 
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
         <ul>
-            <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
-                <a href="" className="flex justify-between items-center w-full text-white">
-                    {""}
-                    <>
-                        LinkedIn <FaLinkedin size={30}/>
-                    </>
-                </a>
-            </li>
+            {links.map(({id,child,href,style,download}) => (
+                <li  className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " " + style}>
+                    <a href={href} className="flex justify-between items-center w-full text-white" download={download} target='_blank' rel="noreferrer">
+                        {child}
+                    </a>
+                </li>
+            ))}
+
+
+            
         </ul>
     </div>
   )
